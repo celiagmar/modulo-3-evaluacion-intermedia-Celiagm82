adcc09d07579730360ccde6cb824ae5723ac1a45
Celiagm82 <celiagm82@gmail.com>
2020-04-07 10:43:07 +0200

    add docs

---
+++ ev-intermedia/docs/precache-manifest.02eb0f9b2680af602136c7d38da3b81f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31c37e68e3aa6a54389224d0e7146623",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b21160b30752db1e5173",
+    "url": "./static/css/main.e955c064.chunk.css"
+  },
+  {
+    "revision": "ed6fb0b563c2cb0ff01d",
+    "url": "./static/js/2.e05283b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.e05283b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b21160b30752db1e5173",
+    "url": "./static/js/main.dd6e3ef8.chunk.js"
+  },
+  {
+    "revision": "7358958c07b6eccee16d",
+    "url": "./static/js/runtime-main.fdf97edd.js"
+  }
+]);
